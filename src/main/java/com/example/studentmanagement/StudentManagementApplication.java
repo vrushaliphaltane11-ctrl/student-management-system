f71f2d0578ac37a3842af0cefc68148870f77a8a
Vrushali Phaltane <vrushaliphaltane11@gmail.com>
2026-06-19 16:05:58 +0530

    Update StudentManagementApplication.java

--- src/main/java/com/example/studentmanagement/StudentManagementApplication.java
+++ src/main/java/com/example/studentmanagement/StudentManagementApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementApplication {
+public class   StudentManagementApplication {
 
     public static void main(String[] args) {
 
